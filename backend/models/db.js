72ecc14f51459b82f29e2735e6dc4057294ac61d
CarlosFullStack <carlos.matelunab@gmail.com>
2018-12-28 12:24:23 -0300

    backend

---
+++ backend/models/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser',true);
+mongoose.set('useCreateIndex',true);
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB connection succeeded.'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+});
+
+require('./user.model');
